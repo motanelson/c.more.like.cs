6daca09b88d517b9950d23efd0eaede83e3861e6
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-06-24 20:16:15 +0100

    Add files via upload

---
+++ clear.c
@@ -0,0 +1,12 @@
+#include "System.h"
+
+int main() {
+    String s="Hello World..."  ;
+    Console_BackgroundColor(Yellow);
+    Console_ForegroundColor(Black);
+    Console_Clear();
+    Console_WriteLine(s);    
+        
+        
+    return 0;
+}
