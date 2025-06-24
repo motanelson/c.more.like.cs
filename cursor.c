ff07bb6ca918914517d57d48b33c8ef3c355b8c0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-06-24 20:34:35 +0100

    Add files via upload

---
+++ cursor.c
@@ -0,0 +1,14 @@
+#include "System.h"
+
+int main() {
+    int i=0;
+    Console_BackgroundColor(Yellow);
+    Console_ForegroundColor(Black);
+    Console_Clear();
+    for(i=0;i<16;i++){
+        Console_SetCursorPosition(i,i+1);
+        Console_Write(ToString(i));    
+    }
+        
+    return 0;
+}
