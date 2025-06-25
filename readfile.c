1cfbed2f38082b0ce27d438e402180e74a8362e1
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-06-25 12:33:42 +0100

    Add files via upload

--- readfile.c
+++ readfile.c
@@ -1,11 +1,11 @@
 #include "System.h"
 
-int main() {
+void Main() {
     String s=File_ReadAllText("out.txt")  ;
     Console_BackgroundColor(Yellow);
     Console_ForegroundColor(Black);
     Console_WriteLine(s);    
     free(s);   
         
-    return 0;
+    
 }
