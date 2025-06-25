bd6d7924b1fb7fef22f7256cb6ac9507dedea349
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-06-25 12:28:06 +0100

    Add files via upload

--- datetime.c
+++ datetime.c
@@ -1,11 +1,11 @@
 #include "System.h"
 
-int main() {
+void Main() {
     String s=DataTime_Now_ToString()  ;
     Console_BackgroundColor(Yellow);
     Console_ForegroundColor(Black);
     Console_WriteLine(s);    
         
         
-    return 0;
+    
 }
